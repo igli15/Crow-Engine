3526013a0e1dec51da5983feaea9a8a81fcb1faa
Igleer <milaqiigli@gmail.com>
2020-04-18 15:52:54 +0200

    documented the transform component

--- Crow/Engine/Components/Transform.h
+++ Crow/Engine/Components/Transform.h
@@ -18,22 +18,41 @@ struct Transform {
 
 public:
 
+    ///Returns the local transformation matrix
     const glm::mat4& GetLocalTransform();
+
+    ///Returns the world transformation matrix
     const glm::mat4& GetWorldTransform();
 
+    ///Translate the transform locally
+    ///@param translation translation vector3
     void Translate(const glm::vec3& translation);
+
+    ///Scale the transform locally
+    ///@param scale scale vector3
     void Scale(const glm::vec3& scale);
+
+    ///Rotate the transform locally
+    ///@param rots rotation quaternion
     void Rotate(const glm::quat& rot);
 
     void SetLocalPosition(const glm::vec3& pos);
     void SetScale(const glm::vec3& scale);
+
+
+    ///Rotate around an axis by an angle
+    ///@param angle the angle in degrees
+    ///@param axis vector3 specifing the axis of rotation
     void Rotate(float angle,const glm::vec3& axis);
+
+
     glm::vec3 LocalPosition();
     glm::vec3 WorldPosition();
 
     void SetParent(Transform* transform);
     int GetChildCount();
 
+    ///Marks all the children entities attached to the transform for destruction.
     void DestroyAllChildrenEntities();
 
 private:
