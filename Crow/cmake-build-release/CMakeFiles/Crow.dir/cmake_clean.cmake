12233921e89224a8d132aa7a60d97d199a60c058
Igleer <milaqiigli@gmail.com>
2020-06-21 18:29:50 +0200

    [Website] Working on Feather section

--- Crow/cmake-build-release/CMakeFiles/Crow.dir/cmake_clean.cmake
+++ Crow/cmake-build-release/CMakeFiles/Crow.dir/cmake_clean.cmake
@@ -1,17 +1,17 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Crow.dir/main.cpp.o"
   "CMakeFiles/Crow.dir/Debug/Debug.cpp.o"
-  "CMakeFiles/Crow.dir/Feather/EntityManager.cpp.o"
   "CMakeFiles/Crow.dir/Feather/ComponentArray.cpp.o"
   "CMakeFiles/Crow.dir/Feather/ComponentManager.cpp.o"
+  "CMakeFiles/Crow.dir/Feather/EntityManager.cpp.o"
+  "CMakeFiles/Crow.dir/Feather/Feather.cpp.o"
   "CMakeFiles/Crow.dir/Feather/System.cpp.o"
   "CMakeFiles/Crow.dir/Feather/SystemManager.cpp.o"
-  "CMakeFiles/Crow.dir/Feather/Feather.cpp.o"
-  "CMakeFiles/Crow.dir/OOP_Engine/Gameobject.cpp.o"
   "CMakeFiles/Crow.dir/OOP_Engine/Component.cpp.o"
+  "CMakeFiles/Crow.dir/OOP_Engine/Gameobject.cpp.o"
   "CMakeFiles/Crow.dir/OOP_Engine/OOPEngine.cpp.o"
-  "Crow.pdb"
+  "CMakeFiles/Crow.dir/main.cpp.o"
   "Crow"
+  "Crow.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
